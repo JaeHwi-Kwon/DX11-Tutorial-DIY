36d8213790656ff4924de790b8d6ef3a92e191df
kdgkt123 <42763934+kdgkt123@users.noreply.github.com>
2022-07-07 00:38:40 +0900

    Popup blank window
    tutorial following #1
    show blank window on window screen

---
+++ DX11 Tutorial Prj/GraphicsClass.h
@@ -0,0 +1,20 @@
+#pragma once
+
+const bool FULL_SCREEN = false;
+const bool VSYNC_ENABLED = true;
+const float SCREEN_DEPTH = 1000.0f;
+const float SCREEN_NEAR = 0.1;
+
+class GraphicsClass {
+public:
+	GraphicsClass();
+	GraphicsClass(const GraphicsClass&);
+	~GraphicsClass();
+
+	bool Initialize(int, int, HWND);
+	void Shutdown();
+	bool Frame();
+
+private:
+	bool Render();
+};
---
+++ DX11 Tutorial Prj/Stdafx.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN
+#include<Windows.h>
+
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+#include "DxDefine.h"
