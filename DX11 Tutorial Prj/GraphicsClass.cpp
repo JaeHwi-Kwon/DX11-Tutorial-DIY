16789b9700c73a45403a98bfed48547fe23fa93a
kdgkt123 <42763934+kdgkt123@users.noreply.github.com>
2022-08-09 23:21:51 +0900

    tutorial6 ex2
    make light green

--- DX11 Tutorial Prj/GraphicsClass.cpp
+++ DX11 Tutorial Prj/GraphicsClass.cpp
@@ -56,7 +56,7 @@ bool GraphicsClass::Initialize(int screenWidth, int screenHeight, HWND hwnd) {
 		return false;
 	}
 
-	m_Light->SetDiffuseColor(1.0f, 0.0f, 1.0f, 1.0f);
+	m_Light->SetDiffuseColor(0.0f, 1.0f, 0.0f, 1.0f);
 	m_Light->SetDirection(0.0f, 0.0f, 1.0f);
 
 	return true;
